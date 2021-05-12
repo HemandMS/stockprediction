89d2a2cea660b9421836e02ca4ae9b13b5724c49
HemandMS <60938141+HemandMS@users.noreply.github.com>
2021-05-12 16:44:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,32 +17,25 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 import datetime
 from datetime import date
 import pandas as pd
-import numpy as np
 import yfinance as yf
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 
 st.title('Stock Forecast App')
-
 st.sidebar.title('Training Data')
 st.sidebar.subheader('Select details:')
 start_date = st.sidebar.date_input("Start date",datetime.date(2015,1,1))
 end_date = st.sidebar.date_input("End date",datetime.date(2021,1,1))
-
 ticker_list = pd.read_csv('https://raw.githubusercontent.com/HemandMS/tickerlist/main/ticker_list.txt')
 ticker_select = st.sidebar.selectbox('Stock ticker', ticker_list)
 tickerData = yf.Ticker(ticker_select)
 string_logo = '<img src=%s>' % tickerData.info['logo_url']
 st.markdown(string_logo, unsafe_allow_html=True)
-
 string_name = tickerData.info['longName']
 st.header('**%s**' % string_name)
-
 string_summary = tickerData.info['longBusinessSummary']
 st.info(string_summary)
-
-
 n_years = st.slider('Years of prediction:', 1, 4)
 period = n_years * 365
 
@@ -59,7 +52,6 @@ data_load_state.text('Loading data... done!')
 st.subheader('Raw data')
 st.write(data.tail())
 
-# Plot raw data
 def plot_raw_data():
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
@@ -69,8 +61,6 @@ def plot_raw_data():
 
 plot_raw_data()
 
-
-# Predict forecast with Prophet.
 df_train = data[['Date', 'Close']]
 df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
@@ -80,10 +70,6 @@ future = m.make_future_dataframe(periods=period)
 forecast = m.predict(future)
 forecast.info()
 
-
-# Show and plot forecast
-#st.subheader('Forecast data')
-#st.write(forecast.tail())
 
 st.subheader(f'Forecast plot for {n_years} years')
 fig1 = plot_plotly(m, forecast)
@@ -101,13 +87,9 @@ def load_real_data(x):
 
 real_data = load_real_data(ticker_select)
 
-# Plot real data
-
 submit = st.sidebar.button('Compare')
-
 if submit:
     st.subheader('Actual data')
-    #st.write(real_data.tail())
     fig3 = go.Figure()
     fig3.add_trace(go.Scatter(x=real_data['Date'], y=real_data['Open'], name="stock_open"))
     fig3.add_trace(go.Scatter(x=real_data['Date'], y=real_data['Close'], name="stock_close"))
@@ -117,5 +99,3 @@ if submit:
 st.write("Forecast components")
 fig2 = m.plot_components(forecast)
 st.write(fig2)
-
-
